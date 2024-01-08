503d073acad61548d31b5352196c77b019ad6310
rohit-sama <rohit9804singh@gmail.com>
2024-01-09 02:58:53 +0530

    removed  Crxjs/vite-plugin

--- costsync/vite.config.js
+++ costsync/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './src/public/manifest.json'
 
 
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest }),
-  ],
+  
 })
